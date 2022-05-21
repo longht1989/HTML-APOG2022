61f62450ec11b0dd629ef83aed834ccf103bc5dc
longht1989 <thanhlong.vn09@gmail.com>
2022-05-22 02:54:33 +0700

    update

---
+++ App_themes/js/app.js
@@ -0,0 +1,42 @@
+// init function
+var isMobile, isTablet, isDesktop;
+
+$(function() {
+    /*check device width*/
+    bsContainerWidth = $("body").find('.container-xxl').outerWidth();
+    if (bsContainerWidth < 768) {
+        console.log("mobile");
+        isMobile = true;
+    } else if (bsContainerWidth < 960) {
+        console.log("tablet");
+        isTablet = true;
+    } else {
+        console.log("desktop");
+        isDesktop = true;
+    }
+    $("#site-header .m-btn").on('click', btnExpandClick);
+    /*pin header */
+    window.onscroll = function() { windowScroll() };
+});
+
+/*customise function*/
+// scoll down & pin site header
+function windowScroll() {
+    var headerHeight = $("#site-header").outerHeight();
+    if (document.body.scrollTop > (headerHeight + 20) || document.documentElement.scrollTop > (headerHeight + 20)) {
+        $("#site-header").addClass('is-pinned');
+    } else {
+        $("#site-header").removeClass('is-pinned');
+    }
+}
+
+function btnExpandClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    expandNav();
+}
+
+function expandNav() {
+    $('#site-header .m-btn').toggleClass('is-active');
+    $('#site-header .nav').toggleClass('is-active');
+}
